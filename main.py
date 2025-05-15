822c5448af12cdcc77ee0fc519b6c2800b8895a8
Aizenda <qaz098380740669@gmail.com>
2025-05-16 00:15:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import *
+from fastapi.responses import FileResponse
+from backend.route import index
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static" ,StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def home_page():
+	file_path = "./static/html/index.html"
+	return FileResponse(file_path, media_type="text/html")
+
+@app.get("/privateAdoption")
+async def private_page():
+	file_path = "./static/html/privateAdoption.html"
+	return FileResponse(file_path, media_type="text/html")
+
+@app.get("/publicAdoption")
+async def public_page():
+	file_path = "./static/html/publicAdoption.html"
+	return FileResponse(file_path, media_type="text/html")
+
+@app.get("/sendAdoption")
+async def send_page():
+	file_path = "./static/html/sendAdoption.html"
+	return FileResponse(file_path, media_type="text/html")
+
+@app.get("/loginButton")
+async def login_page():
+	file_path = "./static/html/login.html"
+	return FileResponse(file_path, media_type="text/html")
---
+++ static/js/header.js
@@ -0,0 +1,45 @@
+const headerModel = {
+	
+}
+
+const headerView = {
+	element : {
+		homeLogo:document.querySelector(".logo"),
+		privateAdoption:document.querySelector("#privateAdoption"),
+		publicAdoption:document.querySelector("#publicAdoption"),
+		sendAdoption:document.querySelector("#sendAdoption"),
+		loginButton:document.querySelector("#loginButton")
+	}
+}
+
+const headerControl = {
+	init(){
+		headerView.element.homeLogo.addEventListener("click",()=>{
+			this.goPath("/");
+		});
+
+		headerView.element.privateAdoption.addEventListener("click",()=>{
+			this.goPath("/privateAdoption");
+		})
+
+		headerView.element.publicAdoption.addEventListener("click",()=>{
+			this.goPath("/publicAdoption");
+		})
+
+		headerView.element.sendAdoption.addEventListener("click",()=>{
+			this.goPath("/sendAdoption");
+		})
+
+		headerView.element.loginButton.addEventListener("click",()=>{
+			this.goPath("/loginButton");
+		})
+	},
+
+	goPath(url){
+		location.href = url;
+	}
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+	headerControl.init();
+});
